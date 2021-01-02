a0e508bf9e5884c2769ce3fc5f390219748af883
masayamatu <m.masa1104@outlook.jp>
2021-01-02 22:48:49 +0900

    R030102

---
+++ leetcode learn/binarytree/ch1-2.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        def inorder(node,ans):
+            if node == None:
+                return None
+            inorder(node.left,ans)
+            ans.append(node.val)
+            inorder(node.right,ans)
+        ans = []
+        inorder(root,ans)
+        return ans
